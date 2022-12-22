a41efe713381ae0949ed9bfb7e9db6df7795a15b
abdullakhan8999 <abdullakhan8999@gmail.com>
2022-12-22 21:41:14 +0530

    Todo App

---
+++ script.js
@@ -0,0 +1,31 @@
+// get Element by id
+// const input = document.getElementById("todoInput");
+// input.placeholder = "New Todo";
+// input.style.backgroundColor = "black";
+// input.style.color = "white";
+
+// get Element by Class
+// const inputColor = document.getElementsByClassName("form-control");
+// for (const iterator of inputColor) {
+//    iterator.style.backgroundColor = "grey";
+//    iterator.style.color = "White";
+// }
+
+// get element by tag name
+// const todoApp = document.getElementsByTagName("h1");
+// for (const iterator of todoApp) {
+//   iterator.style.color = "black";
+// }
+
+// get element by query selector
+// first element will return
+// const todos = document.querySelector(".list-group-item");
+// todos.style.backgroundColor = "green";
+
+// get element by query selectorAll
+// this will give all element
+// const li = document.querySelectorAll("li");
+// console.log(li);
+// for (const ele of li) {
+//   ele.style.backgroundColor = "green";
+// }
