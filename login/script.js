b7e2a10050bfc291858ea588813e2d7af63fac26
Patan AbdullaKhan <111234279+abdullakhan8999@users.noreply.github.com>
2022-12-30 09:45:59 +0530

    SignUp | Login Page

---
+++ login/script.js
@@ -0,0 +1,20 @@
+//variable
+
+//signUP variable
+const SignUp_UserName = getElementId("SignUp_UserName");
+
+const SignUp_UserName = getElementId("SignUp_UserName");
+
+// login variable
+const loginUserName = getElementId("loginUserName");
+
+const loginPassword = getElementId("loginPassword");
+
+const loginBtn = getElementId("loginBtn");
+
+const signUpLink = getElementId("signUpLink");
+
+//function
+function getElementId(id) {
+  return document.getElementById(id);
+}
