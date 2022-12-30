5a2e9f957faf3d54c930e186c2cc512983a3dde4
Patan AbdullaKhan <111234279+abdullakhan8999@users.noreply.github.com>
2022-12-30 09:58:01 +0530

    Add files via upload

---
+++ login/script.js
@@ -0,0 +1,38 @@
+//variable
+
+//signUP variable
+const SignUp_UserName = getElementId("SignUp_UserName");
+
+const SignUp_email = getElementId("SignUp_email");
+
+const SignUp_Password = getElementId("SignUp_Password");
+
+const SignUp_RepeatPassword = getElementId("SignUp_RepeatPassword");
+
+const SignUp_terms = getElementId("SignUp_terms");
+
+const SignUp_Btn = getElementId("SignUp_Btn");
+
+const Login_Link = getElementId("Login_Link");
+
+// login variable
+const loginUserName = getElementId("loginUserName");
+
+const loginPassword = getElementId("loginPassword");
+
+const loginBtn = getElementId("loginBtn");
+
+const signUpLink = getElementId("signUpLink");
+
+//event 
+signUpLink.addEventListener("click", showSignup);
+
+Login_Link.addEventListener("click", showLogin);
+
+
+//function
+function getElementId(id) {
+  return document.getElementById(id);
+}
+
+signUpLink.append
